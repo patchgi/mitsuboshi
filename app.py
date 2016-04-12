8ada77684a789a3c990bfe7d7f1a27c875ab89aa
patchgi <pacchigi0910@gmail.com>
2016-04-12 10:53:51 +0900

    コード修正

--- app.py
+++ app.py
@@ -4,18 +4,13 @@ if __name__=="__main__":
 	result=""
 
 	while result!="012":
-		rand=str(random.randint(0,2))
+		rand=random.randint(0,2)
 		if len(result)==3:
-			result=result[1:]+rand
+			result=result[1:]+str(rand)
 		else:
-			result+=rand
-		if rand=="0":
-			print ("燃やせ")
-		elif rand=="1":
-			print ("友情")
-		else:
-			print ("パッションは")
-	
-	print ("ミツボシ☆☆★")
+			result+=str(rand)
+
+		print(u"燃やせ",u"友情",u"パッションは")[rand]
+	print (u"ミツボシ☆☆★")
 
 	
