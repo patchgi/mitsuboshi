df93371e7bff8eefe4293f7dcc864132fb680a23
patchgi <pacchigi0910@gmail.com>
2016-04-12 10:56:35 +0900

    コード修正

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 #coding:utf-8
 import random
 if __name__=="__main__":
-	result=""
+	result="000"
 
 	while result!="012":
 		rand=random.randint(0,2)
-		if len(result)==3:
-			result=result[1:]+str(rand)
-		else:
-			result+=str(rand)
-
+		result=result[1:]+str(rand)
 		print(u"燃やせ",u"友情",u"パッションは")[rand]
 	print (u"ミツボシ☆☆★")
 
